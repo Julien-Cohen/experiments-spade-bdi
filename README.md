8d5331f37b3e4a1b4a2e7f371d68554c74091537
Julien Cohen <Julien.Cohen@univ-nantes.fr>
2025-07-25 10:09:25 +0200

    Put server address and agent password in environment variables.

--- README.md
+++ README.md
@@ -2,16 +2,27 @@
 
 ## Installation tips
 
-Fix (see https://github.com/javipalanca/spade/issues/127)
+The latest versions of some packages break spade (see https://github.com/javipalanca/spade/issues/127). So use those
+ones instead:
 
+```
 spade==4.0.3
-
 pyjabber==0.2.4
-
 slixmpp==1.9.1
+```
+
+## Configuration
+
+Add in your environment (shell or .env file) two variables to set the address of the chat server (see below) and the
+password of your agent on the chat server (see below).
+
+```
+XMPP_ADDR=...
+AGENT_PASSWD=...
+```
 
 ## Run tips
 
-Reminder : run spade (see prepare.sh)
+Reminder (from Spade): launch the chat server before running the agent with run spade (see prepare.sh)
 
-Tip : On first connexion of an agent to the server, the user is created and the password is set.
+Tip : On the first connexion of an agent to the chat server, the user is created and the password is set.
--- run_example.py
+++ run_example.py
@@ -6,22 +6,57 @@ import spade
 
 from spade_bdi.bdi import BDIAgent
 
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+
+tmp_addr = os.getenv("XMPP_ADDR")
+chat_server_address = tmp_addr if tmp_addr else "0.0.0.0"
+
+tmp_passwd = os.getenv("AGENT_PASSWD")
+agent_password = tmp_passwd if tmp_passwd else ""
+
+
+def print_state(a):
+    print("CURRENT_BELIEFS:" + str(a.bdi.get_beliefs()))
+
+
+def add(a, k, v):
+    print("ADDING BELIEFS: " + k + "=" + v)
+    a.bdi.set_belief(k, v)
+
+
+def remove(a, k, v):
+    print("REMOVING BELIEFS: " + k + "=" + v)
+    a.bdi.remove_belief(k, v)
+
+
+async def start(a):
+    print("STARTING AGENT")
+    await a.start()
+
 
 async def main(server, password):
     a = BDIAgent(f"bdiagent@{server}", password, "basic.asl")
-    await a.start()
+    print_state(a)
+    add(a, "sun", "white")
+    print_state(a)
+    await start(a)
 
     await asyncio.sleep(1)
 
-    a.bdi.set_belief("car", "azul", "big")
-    a.bdi.print_beliefs()
-    print("GETTING FIRST CAR BELIEF")
-    print(a.bdi.get_belief("car"))
-    a.bdi.print_beliefs()
-    a.bdi.remove_belief("car", "azul", "big")
-    a.bdi.print_beliefs()
-    print(a.bdi.get_beliefs())
-    a.bdi.set_belief("car", "amarillo")
+    print_state(a)
+
+    remove(a, "car", "red")
+    await start(a)
+    print_state(a)
+    add(a, "car", "yellow")
+    add(a, "car", "green")
+    print_state(a)
+    add(a, "sun", "yellow")
+    await start(a)
 
     await a.stop()
 
@@ -33,15 +68,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.server is None:
-        print(
-            "Ensure that the spade server is run, then enter its address here (by default the server is run at 0.0.0.0)."
-        )
-        server = input("XMPP Server> ")
+        server = chat_server_address
     else:
         server = args.server
 
     if args.password is None:
-        passwd = getpass.getpass()
+        passwd = agent_password
     else:
         passwd = args.password
     spade.run(main(server, passwd))
